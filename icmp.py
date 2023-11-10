b936dfe4795fc0edea09a9e8c40c4216e97027ab
Andrew Stringer <andrew@rainsbrook.co.uk>
2023-11-10 14:39:11 +0000

    Update icmp.py
    Mods to fix json format to make it compliant, python style improvements.

--- icmp.py
+++ icmp.py
@@ -1,28 +1,31 @@
 from icmplib import ping
 import paho.mqtt.client as paho
-import config
-import time
+import config  # Import config file for variables
 import sys
 import json
 
-def ping_host(host):
-    pingresponse = ping(host, count=1, timeout=2, privileged=True )
-    print("Is alive?", pingresponse.is_alive)
-    return pingresponse
 
-def publish_to_mqtt(pubtopic, payload, qos):
-    print("From in publish_to_mqtt", pubtopic, payload, qos)
-    clientpub.publish(pubtopic, payload, qos)
+def ping_host(host):
+    ping_response = ping(host, count=1, timeout=2, privileged=False )
+    # print("Is alive?", host, ping_response.is_alive)
+    return ping_response
+
+
+def publish_to_mqtt(pub_topic, payload, qos):
+    # print("From in publish_to_mqtt", pub_topic, payload, qos)
+    client_pub.publish(pub_topic, payload, qos)
     return
 
+# Example output:-
+# ["Internet_Status", {"internet_ok": true}, {"1.1.1.1": {"is_alive": true, "rtt": 13.46}}, {"8.8.4.4": {"is_alive": true, "rtt": 13.343}}]
 
-# main code
-clientpub = paho.Client("clientsub-002")
-clientpub.connect(config.broker)
-print("publishing to ", config.publish_topic)
+# Main code
+client_pub = paho.Client("clientsub-002")
+client_pub.connect(config.broker)
+# print("publishing to ", config.publish_topic)
 
 
-print("ping to ", config.ipaddress_1)
+# print("ping to ", config.ipaddress_1)
 
 hostalive_1 = ping_host(config.ipaddress_1)
 hostalive_2 = ping_host(config.ipaddress_2)
@@ -32,15 +35,16 @@ hostalive_2 = ping_host(config.ipaddress_2)
 # if both fail, retest both, if both fail again, then assume internet is down.
 
 if hostalive_1.is_alive or hostalive_2.is_alive:
-    print("1st test - Either hostalive_1 or 2 alive, 1:-", hostalive_1.is_alive, "2:-", hostalive_2.is_alive)
+    # print("1st test - Either hostalive_1 or 2 alive, 1:-", hostalive_1.is_alive, "2:-", hostalive_2.is_alive)
     host1 = {hostalive_1.address:{ "is_alive": hostalive_1.is_alive, "rtt": hostalive_1.min_rtt }}
     host2 = {hostalive_2.address:{ "is_alive": hostalive_2.is_alive, "rtt": hostalive_2.min_rtt }}
     teststatus = {"internet_ok":True}
     message = ["Internet_Status", teststatus, host1, host2]
 
-    print(message)
+    result = json.dumps(message)
+    print(result)
     # publish internet is OK
-    publish_to_mqtt(config.publish_topic,  json.dumps(message), 0)
+    publish_to_mqtt(config.publish_topic, result, 0)
     sys.exit(0)
 else:
     # 2nd test
@@ -51,22 +55,22 @@ else:
     host_retest_2 = {hostalive_retest_2.address: {"is_alive": hostalive_retest_2.is_alive, "rtt": hostalive_retest_2.min_rtt}}
 
     if hostalive_retest_1.is_alive or hostalive_retest_2.is_alive:
-        print("retest - Either hostalive_retest_1 or 2 alive, 1:-", hostalive_retest_1, "2:-", hostalive_retest_2.is_alive)
+        # print("retest - Either hostalive_retest_1 or 2 alive, 1:-", hostalive_retest_1, "2:-", hostalive_retest_2.is_alive)
         reteststatus = {"internet_ok":True}
         message_retest = ["Internet_Status", reteststatus, host_retest_1, host_retest_2]
 
-        print(message_retest)
+        retest_result = json.dumps(message_retest)
+        print(retest_result)
         # publish internet is OK
-        publish_to_mqtt(config.publish_topic,  json.dumps(message_retest), 0)
+        publish_to_mqtt(config.publish_topic, retest_result, 0)
         sys.exit(0)
     else:
-        print("Failed test and retest")
-        reteststatus = {"internet_ok":False}
+        # print("Failed test and retest")
+        reteststatus = {"internet_ok":"False"}
         message_retest = ["Internet_Status", reteststatus, host_retest_1, host_retest_2]
 
-        print(message_retest)
+        retest_result = json.dumps(message_retest)
+        print(retest_result)
         # publish internet is NOT ok
-        publish_to_mqtt(config.publish_topic,  json.dumps(message_retest), 0)
-
+        publish_to_mqtt(config.publish_topic, retest_result, 0)
         sys.exit(1)
-
