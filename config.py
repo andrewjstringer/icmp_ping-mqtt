583612d746f310e11a6cc16f59bf3f4b01387cf9
Andrew Stringer <andrew@rainsbrook.co.uk>
2023-11-10 14:35:45 +0000

    Update config.py
    Changed default servers to ones which should work.

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ brokerqos = 0
 # ip to test settings
 # Should return ok, cloudflare dns and google dns
 ipaddress_1 = '1.1.1.1'
-# ipaddress_2 = '8.8.4.4'
+ipaddress_2 = '8.8.4.4'
 
 # testing addresses - these should fail
 # ipaddress_1 = '1.2.3.4'
-ipaddress_2 = '2.3.4.5'
+# ipaddress_2 = '2.3.4.5'
 
